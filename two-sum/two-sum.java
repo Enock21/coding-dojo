9c19efdb6b11d99f92f9da21d615f4b1f51f03be
enock21 <enock.souza@ccc.ufcg.edu.br>
2025-05-26 15:45:14 -0300

    resolvendo incompatibilidade de commit

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -39,34 +39,47 @@ class Solution {
      */
 
     /* Solução com hashmap e dois laços*/
-    public static int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++){
-            map.put(nums[i], i);
-        };
-        for (int i = 0; i < nums.length; i++){
-            int complement = target - nums[i];
-            if(map.containsKey(complement) && map.get(complement) != i){
-                return new int[]{i, map.get(complement)};
-            }
-        }
-        return new int[]{};
-    }
-
-    /* Solução com hashmap e um laço 
-     * É possível fazer funcionar com valor sendo valor e índice sendo chave no hashmap?
-    */
-    // public static int[] twoSum(int[] nums, int target){
+    // public static int[] twoSum(int[] nums, int target) {
     //     Map<Integer, Integer> map = new HashMap<>();
-    //     for(int i = 0; i < nums.length; i++){
+    //     for (int i = 0; i < nums.length; i++){
+    //         map.put(nums[i], i);
+    //     };
+    //     for (int i = 0; i < nums.length; i++){
     //         int complement = target - nums[i];
-    //         if (map.containsKey(complement)){
+    //         if(map.containsKey(complement) && map.get(complement) != i){
     //             return new int[]{i, map.get(complement)};
     //         }
-    //         map.put(nums[i], i);
     //     }
     //     return new int[]{};
     // }
+
+    /* Solução com hashmap e um laço 
+     * É possível fazer funcionar com valor do array nums sendo valor no hashmap map e índice do array nums sendo chave no hashmap map?
+    */
+    public static int[] twoSum(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if (map.containsKey(complement)){
+                return new int[]{i, map.get(complement)};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+    /*
+    OBS1: Map é construído como uma estrutura de pilha? NÃO! Parece que ele itera sobre os elementos na ordem inversa as vezes. Mas na verdade essa ordem parece aleatória e é sujeita a mudanças conforme o código executa.
+
+    OBS2: A ordem dos valores retornados pode sair diferente nessa versão
+
+    E se invertermos o papel das chaves e valores?
+    map.put(i, nums[i]);  // índice como chave, valor como valor
+    Isso não funciona. Veja por quê:
+
+    O objetivo do hashmap nesse problema é encontrar rapidamente o índice de um valor que completa o target.
+
+    Se usamos o índice como chave, perdemos a capacidade de buscar rapidamente um valor (por exemplo, map.containsKey(complement) se torna impossível, porque complement seria um valor, não um índice).
+    */
 }
 
 class Main{
