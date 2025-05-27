99669099e25e394cfc6c1d99c738e99a24c70047
enock21 <enock.souza@ccc.ufcg.edu.br>
2025-05-27 15:34:10 -0300

    roman to integer concluído

--- roman-to-integer/main.java
+++ roman-to-integer/main.java
@@ -1,30 +1,99 @@
-class Solution {
+/*
+ * https://leetcode.com/problems/roman-to-integer/description/?envType=problem-list-v2&envId=string
+ */
+
+class Solution1 {
     public static int romanToInt(String s) {
         Integer result = 0;
         String aux = "";
         for (int i = 0; i < s.length(); i++){
-            if (s.charAt(i) == 'I'){
-                if (aux == ""){aux = aux + 'I';};
-                else{result++;};
-            };
-            if (s.charAt(i) == 'V'){
-                if (aux == "I"){
-                    result+=4;
+            char c = s.charAt(i);
+
+            if (aux == "I"){
+                if (c == 'V'){result += 4;}
+                else if(c == 'X'){result += 9;}
+                else{
+                    result++;
                     aux = "";
-                }else {result+=5};
+                }
+            }
+
+            if (aux == "X"){
+                if (c == 'L'){result += 40;}
+                else if(c == 'C'){result += 90;}
+                else{
+                    result+=10;
+                    aux = "";
+                }
+            }
+
+            if (aux == "C"){
+                if(c == 'D'){result += 400;}
+                else if(c == 'M'){result += 900;}
+                else{
+                    result+=100;
+                    aux = "";
+                }
+            }
+
+            if (c == 'I'){
+                aux = "I";
             };
-            if (s.charAt(i) == 'X'){result+=10;};
-            if (s.charAt(i) == 'L'){result+=50;};
-            if (s.charAt(i) == 'C'){result+=100;};
-            if (s.charAt(i) == 'D'){result+=500;};
-            if (s.charAt(i) == 'M'){result+=1000;};
+            if (c == 'V'){result+=5;};
+            if (c == 'X'){
+                aux = "X";
+            };
+            if (c == 'L'){result+=50;};
+            if (c == 'C'){
+                aux = "C";
+            };
+            if (c == 'D'){result+=500;};
+            if (c == 'M'){result+=1000;};
         }
         return result;
     }
 }
 
+class Solution2 {
+    public static Integer romanToInt(String s){
+        Integer result = 0;
+        for (int i = 0; i < s.length(); i++){
+            char c1 = 'a';
+            char c2 = 'a';
+            
+            int currentNum = -1;
+            int nextNum = -2;
+
+            c1 = s.charAt(i);
+            if (i < s.length() - 1) c2 = s.charAt(i+1);
+
+            currentNum = charToNum(c1);
+            if (i < s.length() - 1) nextNum = charToNum(c2);
+
+            if (currentNum < nextNum) {
+                result = result - currentNum;
+            }else{
+                result = result + currentNum;
+            }
+        }
+
+        return result;
+    }
+
+    public static int charToNum(char c){
+        if (c == 'I'){return 1;}
+        else if (c == 'V'){return 5;}
+        else if (c == 'X'){return 10;}
+        else if (c == 'L'){return 50;}
+        else if (c == 'C'){return 100;}
+        else if (c == 'D'){return 500;}
+        else if (c == 'M'){return 1000;}
+        else return -1;
+    }
+}
+
 class Main{
     public static void main(String[] args){
-        System.out.println(Solution.romanToInt("XVIII"));
+        System.out.println(Solution2.romanToInt("IV"));
     }
 }
