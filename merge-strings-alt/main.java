50ed8f1fab824847d3e64807e5e737ac01c28e5c
enock21 <enock.souza@ccc.ufcg.edu.br>
2025-05-28 14:52:20 -0300

    merge strings alt solucao 2 adc, mais eficiente

--- merge-strings-alt/main.java
+++ merge-strings-alt/main.java
@@ -1,8 +1,7 @@
 //https://leetcode.com/problems/merge-strings-alternately/?envType=study-plan-v2&envId=leetcode-75
 
-class Solution {
-    class Solution {
-    public String mergeAlternately(String word1, String word2) {
+class Solution1 {
+    public static String mergeAlternately(String word1, String word2) {
         int minLength = 0;
         int maxLength = 0;
         String result = "";
@@ -23,6 +22,21 @@ class Solution {
         for (int i = minLength; i < maxLength; i++){
             if (!word2Bigger){
                 result = result + word1.charAt(i);
+                /* Problema de eficiência:
+                    Strings em Java são imutáveis.
+
+                    Toda vez que você faz result + something, você está criando uma nova String.
+
+                    Isso exige:
+
+                    Alocar novo espaço na memória
+
+                    Copiar o conteúdo da String anterior
+
+                    Acrescentar o novo caractere
+
+                    🔁 Se você tiver n concatenações, você pode acabar fazendo até O(n²) operações de cópia!
+                 */
             }else {
                 result = result + word2.charAt(i);
             }
@@ -30,10 +44,40 @@ class Solution {
         return result;
     }
 }
+
+class Solution2 {
+    public static String mergeAlternately(String word1, String word2) {
+        int minLength = 0;
+        int maxLength = 0;
+        StringBuilder result = new StringBuilder();
+        boolean word2Bigger = false;
+        if (word1.length() <= word2.length()){
+            minLength = word1.length();
+            maxLength = word2.length();
+            word2Bigger = true;
+        }
+        else{
+            minLength = word2.length();
+            maxLength = word1.length();
+        }
+        for (int i = 0; i < minLength; i++){
+            result.append(word1.charAt(i));
+            result.append(word2.charAt(i));
+        }
+        for (int i = minLength; i < maxLength; i++){
+            if (!word2Bigger){
+                result.append(word1.charAt(i));
+                
+            }else {
+                result.append(word2.charAt(i));
+            }
+        }
+        return result.toString();
+    }
 }
 
 class Main {
     public static void main(String[] args){
-        System.out.println(Solution.mergeAlternately("abcdefg", "null"));
+        System.out.println(Solution2.mergeAlternately("abcdefg", "null"));
     }
 }
