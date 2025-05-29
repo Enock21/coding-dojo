37e742e00f7e8b32b73cd55533644d4e8134c74b
enock21 <enock.souza@ccc.ufcg.edu.br>
2025-05-29 14:25:38 -0300

    move zeroes solucao 2, melhor

--- move-zeroes/main.java
+++ move-zeroes/main.java
@@ -1,7 +1,11 @@
 
-import java.util.Arrays;
+//https://leetcode.com/problems/move-zeroes/submissions/1648178724/?envType=study-plan-v2&envId=leetcode-75
 
-class Solution {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution1 {
     public static void moveZeroes(int[] nums) {
         int a = nums.length -1;
         int b = nums.length -1;
@@ -23,11 +27,31 @@ class Solution {
     }
 }
 
+class Solution2 {
+    public static void moveZeroes(int[] nums) {
+        List<Integer> naoZeros = new ArrayList<>();
+        List<Integer> zeros = new ArrayList<>();
+        int tam = nums.length;
+        for(int i = 0; i < tam; i++){
+            if (nums[i] == 0) zeros.add(0);
+            else naoZeros.add(nums[i]);
+        }
+
+        int i = 0;
+        for(int num : naoZeros){
+            nums[i++] = num;
+        }
+        for(int zero : zeros){
+            nums[i++] = zero;
+        }
+    }
+}
+
 class Main {
     public static int[] nums = new int[]{0,1,0,3,12,0};
 
     public static void main(String[] args){
-        Solution.moveZeroes(nums);
+        Solution2.moveZeroes(nums);
         System.out.println(Arrays.toString(nums));
     }
     
