c46b8c9c02f736d6048df4fb16b7d5b934eeff78
enock21 <enockbezerra21@gmail.com>
2025-05-30 15:48:59 -0300

    Solução 2 adc em 643, aplicando janela deslizante

--- 643_maximum_average_subarray_i/main.java
+++ 643_maximum_average_subarray_i/main.java
@@ -1,6 +1,6 @@
 //https://leetcode.com/problems/maximum-average-subarray-i/?envType=study-plan-v2&envId=leetcode-75
 
-class Solution {
+class Solution1 {
     public static double findMaxAverage(int[] nums, int k) {
         int soma = 0;
         double max = -10000.0;
@@ -20,8 +20,33 @@ class Solution {
     }
 }
 
+class Solution2 {
+    public static double findMaxAverage(int[] nums, int k) {
+        int limiteEsq = 0;
+        int limiteDir = k - 1;
+        int soma = 0;
+        double media;
+        double max;
+
+        for(int i = 0; i <= limiteDir; i++){
+            soma += nums[i];
+        }
+
+        max = (double) soma / k;
+
+        while(limiteDir < nums.length - 1){
+            soma -= nums[limiteEsq++];
+            soma += nums[++limiteDir];
+            media = (double) soma / k;
+            if (media > max) max = media;
+        }
+
+        return max;
+    }
+}
+
 class Main{
     public static void main(String[] args){
-        System.out.println(Solution.findMaxAverage(new int[]{1,12,-5,-6,50,3}, 4));
+        System.out.println(Solution2.findMaxAverage(new int[]{1,12,-5,-6,50,3}, 4));
     }
 }
