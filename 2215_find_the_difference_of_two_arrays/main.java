2694f101a34ed330e17ae23558e9685e8c33963b
enock21 <enock.souza@ccc.ufcg.edu.br>
2025-06-01 15:05:29 -0300

    2215: iniciado

---
+++ 2215_find_the_difference_of_two_arrays/main.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map1 = new HashMap<>();
+        HashMap<Integer, Integer> map2 = new HashMap<>();
+        List<List<Integer>> answer = new ArrayList<List<Integer>>(2);
+        for (int i = 0; i < nums1.length; i++){
+
+        }
+    }
+}
