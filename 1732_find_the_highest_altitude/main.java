7842514a8e8dd2b21fc4b30799d2c586a76b6776
enock21 <enock.souza@ccc.ufcg.edu.br>
2025-06-01 14:50:02 -0300

    1732: solução 2 adicionada, mais eficiente e com melhor design

--- 1732_find_the_highest_altitude/main.java
+++ 1732_find_the_highest_altitude/main.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public static int largestAltitude(int[] gain) {
         int[] result = new int[gain.length + 1];
         int max = -100;
@@ -14,8 +14,21 @@ class Solution {
     }
 }
 
+class Solution2 {
+    public static int largestAltitude(int[] gain) {
+        int max = 0;
+        int soma = 0;
+        for (int i = 0; i < gain.length; i++){
+            soma += gain[i];
+            if (soma > max) max = soma;
+        }
+        return max;
+
+    }
+}
+
 class Main{
     public static void main(String[] args) {
-        System.out.println(Solution.largestAltitude(new int[]{44,32,-9,52,23,-50,50,33,-84,47,-14,84,36,-62,37,81,-36,-85,-39,67,-63,64,-47,95,91,-40,65,67,92,-28,97,100,81}));
+        System.out.println(Solution2.largestAltitude(new int[]{44,32,-9,52,23,-50,50,33,-84,47,-14,84,36,-62,37,81,-36,-85,-39,67,-63,64,-47,95,91,-40,65,67,92,-28,97,100,81}));
     }
 }
