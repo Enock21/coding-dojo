88d525a3e2e905213873d69151139f5e26967df5
enock21 <enock.souza@ccc.ufcg.edu.br>
2025-08-31 09:39:22 -0300

    841: atualizado

--- 841_keys_and_rooms/main.java
+++ 841_keys_and_rooms/main.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 class Solution {
     public static boolean canVisitAllRooms(List<List<Integer>> rooms) {
@@ -25,6 +28,54 @@ class Solution {
     }
 }
 
+class Solution2 {
+    public static boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Set<Integer> roomsVisited = new HashSet<>();
+        Set<Integer> keyStorage = new HashSet<>();
+        visitRoom(0, rooms.get(0), keyStorage, roomsVisited);
+        Iterator<Integer> keyStorageIterator = keyStorage.iterator(); // BUG:
+        /**
+         * Resulta em ConcurrentModificationException na segunda execução de keyStorageIterator.next(), pois o iterator detecta modificação na estrutura do keyStorage, resultado do addAll() em visitRoom. Uma solução é criar um novo Iterator a cada iteração do while, garantindo que o iterator não detecte modificação na estrutura do keyStorage e não lance a exceção.
+         */
+        while(!keyStorage.isEmpty()){
+            Integer keyNumber = keyStorageIterator.next();
+            keyStorageIterator.remove();
+            visitRoom(keyNumber, rooms.get(keyNumber), keyStorage, roomsVisited);
+        }
+        if (roomsVisited.size() == rooms.size()) return true;
+        else return false;
+    }
+
+    public static void visitRoom(Integer roomNumber, List<Integer> keysFound, Set<Integer> keyStorage, Set<Integer> roomsVisited){
+        if (roomsVisited.contains(roomNumber)) return;
+        roomsVisited.add(roomNumber);
+        keyStorage.addAll(keysFound);
+    }
+}
+
+//Aplicando HashSet em vez de ArrayList para otimização.
+class Solution3 {
+    public static boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Set<Integer> roomsVisited = new HashSet<>();
+        Set<Integer> keyStorage = new HashSet<>();
+        visitRoom(0, rooms.get(0), keyStorage, roomsVisited);
+        while(!keyStorage.isEmpty()){
+            Iterator<Integer> keyStorageIterator = keyStorage.iterator();
+            Integer keyNumber = keyStorageIterator.next();
+            keyStorageIterator.remove();
+            visitRoom(keyNumber, rooms.get(keyNumber), keyStorage, roomsVisited);
+        }
+        if (roomsVisited.size() == rooms.size()) return true;
+        else return false;
+    }
+
+    public static void visitRoom(Integer roomNumber, List<Integer> keysFound, Set<Integer> keyStorage, Set<Integer> roomsVisited){
+        if (roomsVisited.contains(roomNumber)) return;
+        roomsVisited.add(roomNumber);
+        keyStorage.addAll(keysFound);
+    }
+}
+
 class Main{
     public static void main(String[] args){
 
@@ -40,6 +91,6 @@ class Main{
         rooms.add(new ArrayList<>(Arrays.asList(2)));
         rooms.add(new ArrayList<>(Arrays.asList(0)));
 
-        System.out.println(Solution.canVisitAllRooms(rooms));
+        System.out.println(Solution3.canVisitAllRooms(rooms));
     }
 }
